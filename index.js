4582926a250a98f1af7d65888a8c60399b052745
joshgit <jschermann91@gmail.com>
2021-12-12 11:38:55 -0600

    prompt team members

--- index.js
+++ index.js
@@ -14,103 +14,116 @@ const Intern = require("./lib/Intern");
 
 let teamMembers = [];
 
-//THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-
 const promptTeam = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "managerName",
-      message: "What is the name of your team Manager?",
-      validate: (managerInput) => {
-        if (managerInput) {
-          return true;
-        } else {
-          console.log("Please enter your team Manager name!");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your team Manager?",
+        validate: (v) => {
+          if (v) {
+            return true;
+          } else {
+            console.log("Please enter your team Manager name!");
+            return false;
+          }
+        },
       },
-    },
 
-    {
-      type: "input",
-      name: "managerId",
-      message: "What is the EMPLOYEE ID of your team Manager?",
-      validate: (employeeIdInput) => {
-        if (employeeIdInput) {
-          return true;
-        } else {
-          console.log("Please enter your team Manager employee ID!");
-          return false;
-        }
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employee ID of your team Manager?",
+        validate: (v) => {
+          if (v) {
+            return true;
+          } else {
+            console.log("Please enter your team Manager employee ID!");
+            return false;
+          }
+        },
       },
-    },
-  ]);
+
+      {
+        type: "input",
+        name: "email",
+        message: "What is the email address of your team Manager?",
+        validate: (v) => {
+          if (v) {
+            return true;
+          } else {
+            console.log("Please enter your team Manager email address.");
+            return false;
+          }
+        },
+      },
+
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the office number of your team Manager?",
+        validate: (v) => {
+          if (v) {
+            return true;
+          } else {
+            console.log("Please enter your team Manager office number.");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((responses) => {
+      let manager = new Manager(
+        responses.name,
+        responses.id,
+        responses.email,
+        responses.officeNumber
+      );
+      teamMembers.push(manager);
+      console.log(teamMembers);
+    });
   // after asking questions, package up manager data as an employee
   // manager extends employee
   // and pushes it into teamMembers[];
   //.then(new manager stuff)
 };
 
-const promptTeamMember = (teamData) => {
-  // If there's no 'team members' array property, create one
-  //   if (!teamData.members) {
-  //     teamData.members = [];
-  //   }
-
-  console.log(`
-  =================
-  Add a New Team Member
-  =================`);
+const promptTeamMember = () => {
   return inquirer
     .prompt([
       {
-        type: "input",
-        name: "name",
-        message: "What is the name of your Team member?",
-        validate: (teamMember) => {
-          if (teamMember) {
-            return true;
-          } else {
-            console.log("Please enter your team member!");
-            return false;
-          }
-        },
-      },
-      {
-        type: "checkbox",
-        name: "languages",
-        message: "What does this guy know? (Check all that apply)",
-        choices: ["choice", "choice2", "choice3"],
-      },
-      {
-        type: "input",
-        name: "link",
-        message: "Enter the GitHub username to your teammate. (Required)",
-        validate: (githubLink) => {
-          if (githubLink) {
-            return true;
-          } else {
-            console.log("Please enter your GitHub username!");
-            return false;
-          }
-        },
-      },
-      {
-        type: "confirm",
-        name: "confirmAddMember",
-        message: "Would you like to enter another team member?",
-        default: false,
+        type: "list",
+        name: "choice",
+        message:
+          "Would you like to add an Engineer, an Intern, or finish the page?",
+        choices: ["Engineer", "Intern", "Finish Page"],
       },
     ])
-    .then((projectData) => {
-      teamData.members.push(projectData);
-      if (projectData.confirmAddMember) {
-        return promptTeamMember(teamData);
-      } else {
-        return teamData;
+    .then((choice) => {
+      switch (choice) {
+        case "Engineer":
+          newEngineer();
+          break;
+        case "Intern":
+          newIntern();
+          break;
+        default:
+          generatePage();
       }
     });
+};
+
+const newEngineer = () => {
+  console.log(teamMembers);
+};
+
+const newIntern = () => {
+  console.log(teamMembers);
+};
+
+const generatePage = () => {
+  console.log(teamMembers);
 };
 
 promptTeam()
